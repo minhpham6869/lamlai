096c1180adfbaafb0e593ba6d79b9d346dd0d2b2
Minh Pham <minhpham6869@gmail.com>
2023-08-05 05:54:59 +0900

    quenmattieu

---
+++ video8/video8.js
@@ -0,0 +1,15 @@
+console.log('xin chao video 8')
+let ojb = {
+    name: 'minh',
+    phone:'0929012329',
+}
+console.log ('what is your name?', ojb.name , 'what is your number phone?', ojb.phone)
+console.log('data type of name', typeof ojb)
+let a = ['mu', 'chelsea', "liver"];
+console.log(`datatype of a`, typeof a,a)
+console.log(`cai1`, a[0], `cai2`, a[2])
+let c = [
+    { ten: 'binh' },
+    { tuoi: `32` }
+]
+console.log(`gia tri cua c`, c)
